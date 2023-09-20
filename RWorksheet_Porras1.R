19b8995f972ddf732ae74c8226155386bcb8988b
Christian Porras <chrispors28@gmail.com>
2023-09-20 04:06:25 +0000

    RWorksheet_Porras1.R

---
+++ RWorksheet_Porras1.R
@@ -0,0 +1,29 @@
+#vector
+age <-c (34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 34, 19, 20,
+         57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41, 51, 35, 24, 33, 41)
+
+#How many data points
+lenght(age)
+
+#reciprocal
+reciprocal <- 1/age
+reciprocal
+
+#new Age
+new_age <- c(age,0,age)
+new_age
+
+#sort the value of age
+sortAge <- sort(age)
+sortAge
+
+#minimum Age
+minAge <- min(age)
+minAge
+
+#maximum Age
+maxAge <- max(age)
+maxAge
+
+
+
