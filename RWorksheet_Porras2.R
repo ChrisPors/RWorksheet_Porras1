23d3cca7f550ff63962b943c7e7f7e4d9ee66062
Christian Porras <chrispors28@gmail.com>
2023-09-28 03:59:40 +0000

    Innitial Commit

---
+++ RWorksheet_Porras2.R
@@ -0,0 +1,27 @@
+newVec <- seq(-5,5)
+newVec
+
+x <- 1:7
+x
+
+VecSeq <- seq(1,3, 0.2)
+VecSeq
+
+Worker_age <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 43, 53, 41, 51, 35, 24, 33, 41, 53, 40, 18, 44, 38, 41, 48, 27, 39, 19, 30, 61, 54, 58, 26, 18)
+Worker_age
+
+third_element <- Worker_age[3]
+third_element
+
+sec_forth <- Worker_age[c(2,4)]
+sec_forth
+
+all_elem <- Worker_age[c(1:3,5:11,13:50)]
+all_elem
+
+other_vec <- c("first" = 3, "second" = 0, "third" = 9)
+other_vec
+
+accessed <- other_vec [c("first", "second")]
+accessed
+
